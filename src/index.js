c792ded3a3ab9bde8b4818189c3a4a27f6ed8c38
vsouza <vinicius.rj@gmail.com>
2017-06-27 09:14:32 -0300

    fix handler names

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import {services,resources} from 'restify-generic-resources';
 import validating from 'restify-sequelize-validation';
 
 export const setupApi = (appAuthSecret, modelName, models) => ({
-    ['findOne_' + modelName]: resources.getModelById(appAuthSecret, null, modelName, models),
+    ['readOne_' + modelName]: resources.getModelById(appAuthSecret, null, modelName, models),
     ['create_' + modelName]: resources.createModel(appAuthSecret, null, modelName, models),
     ['update_' + modelName]: resources.updateModel(appAuthSecret, null, modelName, models),
-    ['listAll_' + modelName]: resources.listAll(appAuthSecret, null, modelName, models),
+    ['readAll_' + modelName]: resources.listAll(appAuthSecret, null, modelName, models),
     ['delete_' + modelName]: resources.deleteModel(appAuthSecret, null, modelName, models)
 });
